369851580e57cf2e4c1ec675ed4b6a82cc305884
Conan Chan <chanconan67@gmail.com>
2020-02-29 23:25:53 -0800

    Completed median of three

--- Algoritms-Divide_And_Conquer/Week3/quicksort.py
+++ Algoritms-Divide_And_Conquer/Week3/quicksort.py
@@ -17,12 +17,13 @@ def quickSortHelp(nums, low, high, pivot):
 def partition(nums, low, high, pivot):
     #pivot = ChoosePivot(nums, low, high
     if pivot == "low":
-        i, comparisons = partitionLow(nums, low, high)
+        i, comparisons = partitionHelp(nums, low, high)
     elif pivot == "high":
-        i, comparisons = partitionHigh(nums, low, high)
+        swap(nums, low, high - 1)
+        i, comparisons = partitionHelp(nums, low, high)
     else:
-        pivot = choosePivot(nums, low, high)
-        i, comparisons = partition(nums, low, high)
+        choosePivot(nums, low, high - 1)
+        i, comparisons = partitionHelp(nums, low, high)
     return i, comparisons
 
 
@@ -31,22 +32,7 @@ def swap(arr, first, last):
     arr[first] = arr[last];
     arr[last] = temp;
 
-def partitionLow(nums, low, high):
-    pivot = low
-    i = low
-    comparisons = high - low - 1
-    for j in range(low, high):
-        if nums[j] == nums[pivot]:
-            i += 1
-            continue
-        elif nums[j] < nums[pivot]:
-            swap(nums, i, j)
-            i += 1
-    swap(nums, i - 1, pivot)
-    return i - 1, comparisons
-
-def partitionHigh(nums, low, high):
-    swap(nums, low, high - 1)
+def partitionHelp(nums, low, high):
     pivot = low
     i = low
     comparisons = high - low - 1
@@ -63,7 +49,6 @@ def partitionHigh(nums, low, high):
 # Take the three indices and swap values so that the value in the middle of the array is the median of values
 # Swap middle value with first element so we can have pivot at the beginning on the list
 def choosePivot(arr, first, last):
-    position = 0
     mid = int((first + last) / 2);
     if arr[first] < arr[last]:
         if arr[first] > arr[mid]:
@@ -79,13 +64,14 @@ def choosePivot(arr, first, last):
             position = last
         else:
             position = mid
-    return position
+    swap(arr, first, position)
 
 
 list = [int(line) for line in open("QuickSort.txt")]
 # list = [4,1,2,5,3]
 #quickSort(list, "low") 
 # A: 162085
-#quickSort(list, "high") 
+# quickSort(list, "high") 
 # A: 164123
-# quickSort(list, "mid")
+quickSort(list, "mid")
+# 138382
