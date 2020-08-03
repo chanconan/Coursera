84c0b9cbc01c15ec4c205c4581b985fd6854d17e
Conan Chan <chanconan67@gmail.com>
2020-08-02 23:26:32 -0700

    Tested example for sending HTTP request

--- py4everyone/chapter12_network/assignment.py
+++ py4everyone/chapter12_network/assignment.py
@@ -3,4 +3,13 @@ import socket
 # connection point that has yet to be connected
 # connect() connects the socket object to the host and the port
 mysocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysocket.connect()
+mysocket.connect(("data.pr4e.org", 80))
+cmd = "GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n".encode()
+mysocket.send(cmd)
+
+while True:
+    data = mysocket.recv(512)
+    if(len(data) < 1):
+        break
+    print(data.decode())
+mysocket.close()
