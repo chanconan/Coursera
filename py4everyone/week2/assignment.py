7e83a3db385fde33a868059c25bbd9540120fb75
Conan Chan <chanconan67@gmail.com>
2020-08-02 22:27:39 -0700

    changed file extension

--- py4everyone/week2/assignment.py
+++ py4everyone/week2/assignment.py
@@ -2,6 +2,6 @@ import re
 def regex_find_sum(text):
     file = open(text)
     print(sum([int(i) for i in (re.findall("[0-9]+", file.read()))]))
-
+    
 print(regex_find_sum("regex_sum_42.txt"))
 print(regex_find_sum("regex_sum_866594.txt"))
